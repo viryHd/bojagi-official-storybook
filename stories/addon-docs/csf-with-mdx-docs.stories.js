148fa5d0112f17fcfa4cfd396fc42b66b4a6d3d8
Simon Males <sime@sime.net.au>
2020-07-28 17:15:46 +0200

    initial commit

---
+++ stories/addon-docs/csf-with-mdx-docs.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@storybook/react/demo';
+
+// NOTE: commented out default since these stories are re-exported
+// in the primary file './csf-docs-with-mdx-docs.stories.mdx'
+//
+// export default {
+//   title: 'Addons/Docs/csf-with-mdx-docs',
+//   component: Button,
+// };
+
+export const Basic = () => <Button>Basic</Button>;
---
+++ stories/addon-jest.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withTests as withTestsHOC } from '@storybook/addon-jest';
+
+import results from './addon-jest.testresults.json';
+
+export default {
+  title: 'Addons/Jest',
+  decorators: [withTestsHOC({ results })],
+};
+
+export const WithTests = () => <p>Hello</p>;
+WithTests.parameters = { jest: 'addon-jest' };
